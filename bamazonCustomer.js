a185595728972b5ce5197f89040ad035b9448761
Max Robb <maxbenrobb@wifi_guest-10-38-126-56.uncc.edu>
2018-03-29 12:06:14 -0400

    working code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,6 +13,8 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+var choiceArray = [];
+
 connection.connect(function(err) {
     if (err) throw err;
     start();
@@ -25,20 +27,55 @@ function start() {
         .prompt([
             {
             name: "choice",
-            type: "list",
+            type: "rawlist",
                 choices: function() {
-                    var choiceArray = [];
                     for (i = 0; i < res.length; i++) {
-                    choiceArray.push(res[i].product_name);
                     choiceArray.push(res[i].product_name);
                     }
                 return choiceArray;
                 },
             message: "What auction would you like to purchase?"
             },       
-        ])
-        connection.end();
+        ]).then(function(value){
+            console.log(value.choice);
+            var number = choiceArray.indexOf(value.choice)
+            quantity(number, res);
+        })
+        //connection.end();
     })
 }
 
-
+function quantity(stock, res) {
+    inquirer
+    .prompt([
+        {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?"
+        }
+    ])
+    .then(function(x) {
+        if (x.quantity > res[stock].stock_quantity) {
+            console.log("Sorry, insufficient quantity!")
+        }
+        else {
+            var updatedQuantity = res[stock].stock_quantity - x.quantity;
+            console.log(updatedQuantity);          
+            connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: updatedQuantity
+                    },
+                    {
+                        item_id: stock
+                    }
+                ],
+            function(err, res) {
+                if (err) throw err;
+                console.log("Order received!");
+                connection.end();
+            });
+            }
+        }
+    )
+}
